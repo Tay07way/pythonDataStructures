64f9bafeb46e516cc6ae0acfe88768e26e5fa5c0
Taylar Conway <PrincessTay@taylars-mbp.mynetworksettings.com>
2022-09-06 13:46:18 -0400

    first commit

---
+++ 03_last_element/last_element.py
@@ -0,0 +1,15 @@
+def last_element(lst):
+    last_element == lst[-1]
+    for elements in lst:
+        return last_element
+    if last_element is "[]" == True:
+        return None
+
+    """Return last item in list (None if list is empty.
+    
+        >>> last_element([1, 2, 3])
+        3
+        
+        >>> last_element([]) is None
+        True
+    """
